bbbe4a022c123ff9fc7a80cd6455bde9467e8e1d
Osama <165610542+Osamahassan2005@users.noreply.github.com>
2024-06-10 12:25:39 +0500

    first commit

---
+++ assigment 1 to 10/main.js
@@ -0,0 +1,43 @@
+"use strict";
+//....AS # 1
+//Install Node.js, TypeScript and VS Code on your computer.
+//..... AS # 2
+let personName = "osama";
+console.log(`Hello ${personName}, would you like to learn some typescipt today?`);
+//......AS # 3
+console.log("lowercase:" + personName.toLowerCase());
+console.log("uppercase:" + personName.toUpperCase());
+let titleCase = personName[0].toUpperCase() + personName.slice(1).toLowerCase();
+console.log("titlecase:" + titleCase);
+//......AS # 4
+let famousPerson = `Dr Abdul Qadeer khan once said,"Pakistan's nuclear program is a symbol of our national 
+resolve and a guarantor of our national security."`;
+console.log(famousPerson);
+//.....AS # 5 
+let famousPersonName = "Dr Abdul Qadeer khan";
+let message = `${famousPersonName} once said ,"Pakistan's nuclear program is a symbol of our national
+ resolve and a guarantor of our national security."`;
+console.log(message);
+//........... AS # 6
+let myName = "my  name is \t osama.\n and i am enrolled in governer initiative";
+console.log(myName);
+//.......AS # 7
+let add = 5 + 5;
+console.log(add);
+let subt = 20 - 10;
+console.log(subt);
+let mult = 5 * 2;
+console.log(mult);
+let divide = 20 / 2;
+console.log(divide);
+//......AS # 9
+let favouriteNumber = 2;
+let newMessage = `Hi! my name is osama and my favourite number is ${favouriteNumber}`;
+console.log(newMessage);
+//......AS # 10
+//0ne
+//My name is osama
+//Date: 10 jun 2024
+// (as#9) this program stores a favourite number in a variable.
+//two
+//this program calculate methametical operators and eash answer is 10
